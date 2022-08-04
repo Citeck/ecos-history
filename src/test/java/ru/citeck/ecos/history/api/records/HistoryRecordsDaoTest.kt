665e02df966258c29eefd7f6d4d2bc8eee59497e
Pavel Simonov <pavel.simonov@citeck.ru>
2022-08-04 17:38:44 +0700

    ECOSCOM-4664 - Fix NPE for jackson NullNode

--- src/test/java/ru/citeck/ecos/history/api/records/HistoryRecordsDaoTest.kt
+++ src/test/java/ru/citeck/ecos/history/api/records/HistoryRecordsDaoTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import ru.citeck.ecos.history.domain.HistoryRecordEntity
 import ru.citeck.ecos.history.repository.HistoryRecordRepository
 import ru.citeck.ecos.records2.predicate.model.Predicates
 import ru.citeck.ecos.records3.RecordsService
@@ -30,28 +29,35 @@ class HistoryRecordsDaoTest {
     @Test
     fun test() {
 
-        val rec = records.create("history-record", mapOf(
-            "documentId" to "test/test@test",
-            "creationTime" to System.currentTimeMillis(),
-            "eventType" to "task.assigned",
-            "userId" to "admin",
-            "username" to "admin",
-            "comments" to "comments"
-        ))
+        val rec = records.create(
+            "history-record",
+            mapOf(
+                "documentId" to "test/test@test",
+                "creationTime" to System.currentTimeMillis(),
+                "eventType" to "task.assigned",
+                "userId" to "admin",
+                "username" to "admin",
+                "comments" to "comments"
+            )
+        )
 
         val query = RecordsQuery.create {
             withSourceId("history-record")
         }
 
-        val recs0 = records.query(query.copy {
-            withQuery(Predicates.`in`("eventType", listOf("task.assigned")))
-        }).getRecords()
+        val recs0 = records.query(
+            query.copy {
+                withQuery(Predicates.`in`("eventType", listOf("task.assigned")))
+            }
+        ).getRecords()
         assertThat(recs0).hasSize(1)
         assertThat(recs0[0]).isEqualTo(rec)
 
-        val recs1 = records.query(query.copy {
-            withQuery(Predicates.`in`("eventType", listOf("unknown")))
-        }).getRecords()
+        val recs1 = records.query(
+            query.copy {
+                withQuery(Predicates.`in`("eventType", listOf("unknown")))
+            }
+        ).getRecords()
         assertThat(recs1).isEmpty()
     }
 }
