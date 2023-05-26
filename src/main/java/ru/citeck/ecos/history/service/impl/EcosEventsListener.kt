276b2ad58ab7897aee066b39878db48a2d81333b
Roman Makarskiy <romanchabest55@gmail.com>
2023-05-26 17:04:19 +0300

    Revert "ECOSCOM-5137 - Dynamic roles based on DMN"
    This reverts commit b16c10bb071f0b05179fc784f7a07f80cade6123.

--- src/main/java/ru/citeck/ecos/history/service/impl/EcosEventsListener.kt
+++ src/main/java/ru/citeck/ecos/history/service/impl/EcosEventsListener.kt
@@ -151,10 +151,8 @@ class EcosEventsListener(
                     }
                     val typeAssoc = typeAssocsById[assoc.assocId]
                     if (typeAssoc != null &&
-                        (
-                            typeAssoc.direction == AssocDef.Direction.BOTH ||
-                                typeAssoc.direction == AssocDef.Direction.TARGET
-                            )
+                        (typeAssoc.direction == AssocDef.Direction.BOTH ||
+                            typeAssoc.direction == AssocDef.Direction.TARGET)
                     ) {
                         assoc.added.forEach { storeSourceAssocHistoryEvent(event, it, typeAssoc, true) }
                         assoc.removed.forEach { storeSourceAssocHistoryEvent(event, it, typeAssoc, false) }
@@ -189,7 +187,7 @@ class EcosEventsListener(
         var comment = targetAssoc.name.getClosest(I18nContext.RUSSIAN).ifBlank { targetAssoc.id } + ": "
         comment += if (added) {
             "добавлен"
-        } else {
+        } else  {
             "удален"
         }
         record[HistoryRecordService.COMMENTS] = comment + " " + sourceUpdatedEvent.recordDisp
