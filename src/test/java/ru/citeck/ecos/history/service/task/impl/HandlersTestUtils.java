a835baa5c20ab40bbccd24ed202ee512ceed975c
Roman Makarskiy <romanchabest55@gmail.com>
2023-08-15 14:11:34 +0300

    refactoring

--- src/test/java/ru/citeck/ecos/history/service/task/impl/HandlersTestUtils.java
+++ src/test/java/ru/citeck/ecos/history/service/task/impl/HandlersTestUtils.java
@@ -1,14 +1,14 @@
 package ru.citeck.ecos.history.service.task.impl;
 
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 import static ru.citeck.ecos.history.service.HistoryRecordService.*;
 
 public class HandlersTestUtils {
 
-    private static final Random random = new Random();
+    private static final SecureRandom random = new SecureRandom();
 
     public static Map<String, String> generateTaskBaseParams(String id) {
         Map<String, String> requestParams = new HashMap<>();
