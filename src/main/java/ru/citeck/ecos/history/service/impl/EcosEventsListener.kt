ff9a3fa32469cefe3b040c849b84d94d756a6794
Roman Makarskiy <romanchabest55@gmail.com>
2024-02-02 23:46:04 +0300

    ECOSENT-2910 - Micrometer metrics for Ecos

--- src/main/java/ru/citeck/ecos/history/service/impl/EcosEventsListener.kt
+++ src/main/java/ru/citeck/ecos/history/service/impl/EcosEventsListener.kt
@@ -73,7 +73,8 @@ class EcosEventsListener(
         }
 
         val excludeBpmnElementsOptimizationPredicate = Predicates.notEq(
-            "record._type?id", "${AppName.EMODEL}/type@bpmn-process-element"
+            "record._type?id",
+            "${AppName.EMODEL}/type@bpmn-process-element"
         )
 
         eventsService.addListener<StatusChanged> {
